ac4584ef2d6be697ae311a431a0f7c47ef7e7220
Victor Han <victor.han99@yahoo.ca>
2021-04-04 17:47:07 -0400

    Java Encryption-init

---
+++ JavaEncryptionDemo/src/org/githubvictorhanxu/encryption/Test01.java
@@ -0,0 +1,7 @@
+package org.githubvictorhanxu.encryption;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("Hello, this is test Java encryption.");
+    }
+}
